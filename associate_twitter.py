0d35684ae0924d4470b487931da29313ae21d419
RCbot <regardscitoyens@gmail.com>
2015-06-22 20:11:59 +0200

    ignore old account gabouty

--- associate_twitter.py
+++ associate_twitter.py
@@ -28,7 +28,7 @@ with open(sys.argv[2]) as f:
 # Exclude bad accounts
 notparls = ["bayrou"]
 groupes = ["crcsenat", "udiuc", "ecolosenat", "senateursps", "senateursump", "lesrep_senat"]
-doublons = ["teambouillon", "fdebeauce", "vignal2012", "deputecvautrin", "clergeau2012", "isabellebruneau", "roussetalain", "elubondy", "FLefebvre_UMP"]
+doublons = ["teambouillon", "fdebeauce", "vignal2012", "deputecvautrin", "clergeau2012", "isabellebruneau", "roussetalain", "elubondy", "FLefebvre_UMP", "Gabouty2012"]
 
 excludes = [t.lower() for t in notparls + groupes + doublons]
 for e in excludes:
