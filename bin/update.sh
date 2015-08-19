e3707247aa59f2e90aa465f4f46e3f8f2d6b004b
RouxRC <b.ooghe@gmail.com>
2015-08-19 11:39:47 +0200

    pull before updating

--- bin/update.sh
+++ bin/update.sh
@@ -2,7 +2,9 @@
 
 cd $(echo $0 | sed 's#/[^/]*$##')/..
 
-bin/build.sh > /tmp/load_twitter_parls.tmp
+git pull > /tmp/load_twitter_parls.tmp
+
+bin/build.sh >> /tmp/load_twitter_parls.tmp
 
 if git status | grep "data" > /dev/null; then
   cat /tmp/load_twitter_parls.tmp
