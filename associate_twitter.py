d655868380fba32830c44486542bb7985c7df8a7
RouxRC <b.ooghe@gmail.com>
2017-02-27 04:02:55 +0100

    remove dead accounts

--- associate_twitter.py
+++ associate_twitter.py
@@ -34,8 +34,9 @@ with open(sys.argv[2]) as f:
 notparls = ["bayrou"]
 groupes = ["crcsenat", "udiuc", "ecolosenat", "senateursps", "senateursump", "lesrep_senat"]
 doublons = ["teambouillon", "fdebeauce", "vignal2012", "deputecvautrin", "clergeau2012", "isabellebruneau", "roussetalain", "elubondy", "FLefebvre_UMP", "Gabouty2012", "moniquerabin", "PascalAllizard", "pascalegruny"]
+dead = ["Guy_Delcourt", "ConchitaLacuey", "MichelVERGNIER", "bernardroman59"]
 
-excludes = [t.lower() for t in notparls + groupes + doublons]
+excludes = [t.lower() for t in notparls + groupes + doublons + dead]
 for e in excludes:
     if e in twitter:
         twitter.pop(e)
