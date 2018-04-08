f0943a1c0b9680cee59161e8481ab7006309266c
RCbot <regardscitoyens@gmail.com>
2018-04-08 23:48:07 +0200

    change source list twitter députés

--- bin/build.sh
+++ bin/build.sh
@@ -7,7 +7,7 @@ echo "--------------"
 source /usr/local/bin/virtualenvwrapper.sh
 workon twitter-parls
 #./download_twitter.py "AssembleeNat" "les-députés"
-./download_twitter.py "TwitterPolFR" "d-put-s-2017-22"
+./download_twitter.py "lcp" "d-put-s-xve-l-gislature"
 ./download_twitter.py "Senat" "senateurs"
 curl -sL "http://www.nosdeputes.fr/deputes/json" > .cache/deputes.json
 curl -sL "http://www.nossenateurs.fr/senateurs/json" > .cache/senateurs.json
@@ -16,7 +16,7 @@ echo
 echo "Associating..."
 echo "--------------"
 #./associate_twitter.py deputes .cache/twitter-AssembleeNat.json
-./associate_twitter.py deputes .cache/twitter-TwitterPolFR.json
-./associate_twitter.py senateurs .cache/twitter-Senat_Info.json
+./associate_twitter.py deputes .cache/twitter-lcp.json
+./associate_twitter.py senateurs .cache/twitter-Senat.json
 echo
 
