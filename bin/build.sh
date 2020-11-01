f20682924d32fbc73b51e008dacedea8949f92c0
RCbot <regardscitoyens@gmail.com>
2020-11-02 00:43:13 +0100

    handle Twitter list IDs instead of slugs + update Senate's list id

--- bin/build.sh
+++ bin/build.sh
@@ -8,7 +8,7 @@ source /usr/local/bin/virtualenvwrapper.sh
 workon twitter-parls
 #./download_twitter.py "AssembleeNat" "les-députés"
 ./download_twitter.py "lcp" "d-put-s-xve-l-gislature"
-./download_twitter.py "Senat" "s-natrices-et-s-nateurs"
+./download_twitter.py "Senat" 21207448
 curl -sL "http://www.nosdeputes.fr/deputes/json" > .cache/deputes.json
 curl -sL "http://www.nossenateurs.fr/senateurs/json" > .cache/senateurs.json
 
--- download_twitter.py
+++ download_twitter.py
@@ -9,7 +9,7 @@ if len(sys.argv) < 3:
     sys.stderr.write("Please input both Twitter list's owner_screen_name and slug\n")
     exit(1)
 
-LIST_USER, LIST_SLUG = sys.argv[1:3]
+LIST_USER, LIST_ID = sys.argv[1:3]
 
 if not os.path.isdir(".cache"):
     os.makedirs(".cache")
@@ -17,19 +17,29 @@ if not os.path.isdir(".cache"):
 t = Twitter(auth=OAuth(OAUTH_TOKEN, OAUTH_SECRET, KEY, SECRET))
 accounts = {}
 page = 1
-cursor = -1
-while cursor:
-    res = t.lists.members(owner_screen_name=LIST_USER, slug=LIST_SLUG, cursor=cursor, include_entities='false', skip_status='true', count=5000)
-    with open(os.path.join('.cache', 'twitter-%s-%s.json' % (LIST_USER, cursor if cursor != -1 else 0)), 'w') as f:
+args = {
+    "owner_screen_name": LIST_USER,
+    "include_entities": "false",
+    "skip_status": "true",
+    "count": 5000,
+    "cursor": -1
+}
+try:
+    args["list_id"] = long(LIST_ID)
+except:
+    args["slug"] = LIST_ID
+while args["cursor"]:
+    res = t.lists.members(**args)
+    with open(os.path.join('.cache', 'twitter-%s-%s.json' % (LIST_USER, args["cursor"] if args["cursor"] != -1 else 0)), 'w') as f:
         json.dump(res, f)
-    cursor = res.get('next_cursor', res.get('next_cursor_str', 0))
+    args["cursor"] = res.get('next_cursor', res.get('next_cursor_str', 0))
     new = 0
     for account in res['users']:
         name = account['screen_name'].lower()
         if name not in accounts:
             accounts[name] = account
             new += 1
-    print("[INFO/%s] page %s -> %s results including %s new ; new total: %s" % (LIST_SLUG, page, len(res['users']), new, len(accounts)))
+    print("[INFO/%s] page %s -> %s results including %s new ; new total: %s" % (LIST_ID, page, len(res['users']), new, len(accounts)))
     page += 1
 
 with open(os.path.join('.cache', 'twitter-%s.json' % LIST_USER), 'w') as f:
