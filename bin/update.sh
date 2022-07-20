fbc9412a9762c75dc0389cb069008f2df341915f
RouxRC <b.ooghe@gmail.com>
2022-07-20 16:29:00 +0200

    sorted log

--- bin/update.sh
+++ bin/update.sh
@@ -4,7 +4,7 @@ cd $(echo $0 | sed 's#/[^/]*$##')/..
 
 git stash && git pull && git stash pop > /tmp/load_twitter_parls.tmp
 
-bin/build.sh >> /tmp/load_twitter_parls.tmp
+bin/build.sh >> /tmp/load_twitter_parls.tmp 2>&1
 
 if git status | grep "data" > /dev/null; then
   cat /tmp/load_twitter_parls.tmp
